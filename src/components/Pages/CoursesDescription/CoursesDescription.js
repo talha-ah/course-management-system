40c9e09c2c0f62a7593ac8fabfb57eb3986bbaa8
Talha <talha1392.ahmed@gmail.com>
2020-02-06 23:14:25 -0800

    NCEAC Done

--- src/components/Pages/CoursesDescription/CoursesDescription.js
+++ src/components/Pages/CoursesDescription/CoursesDescription.js
@@ -1,20 +1,141 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './CoursesDescription.module.css';
+import Button from '../../UI/Button/Button';
+import TextArea from '../../UI/TextArea/TextArea';
 
-const CoursesDescription = () => {
-  return (
-    <div className={classes.CoursesDescription}>
-      <div className={classes.coursesDescriptionHeader}>
-        <h3>CoursesDescription Page</h3>
-        <p>
-          This is your CoursesDescription page. You can see the progress you've
-          made with your coursesDescription here and manage them
-        </p>
+class CoursesDescription extends Component {
+  state = {
+    r1: '',
+    r2: '',
+    r3: '',
+    r4: '',
+    r5: '',
+    r6: '',
+    r7: '',
+    r8: '',
+    r9: '',
+    r10: '',
+    r11: '',
+    r12: '',
+    r13: '',
+    r14: '',
+    r15: '',
+    r16: '',
+    data: {
+      0: {
+        1: 'Course Code',
+        2: 'Course Title',
+        3: 'Credit Hourse',
+        4: 'Prerequisites by Course(s) and Topics',
+        5: 'Assessment Instruments with Weight (homework, quizzes, midterms, final, programming assignments, lab work, etc',
+        6: 'Course Coordinator',
+        7: 'URL (if any)',
+        8: 'Current Catalog',
+        9: ' Textbook (or Laboratory Manual for Laboratory Courses)',
+        10: 'Reference Material',
+        11: 'Course Goals',
+        12: 'Topic Covered in the Course, with Number of Lectures on Each Topic (assume 15-week instruction and one-hour lectures)',
+        13: 'Laboratory Projects/Experiments Done in the Course',
+        14: 'Programming Assignments Done in the Course',
+        15: 'Class Time Spent on (in credit hourse)',
+        16: 'Oral and Written Communications'
+      }
+    }
+  };
+
+  onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
+  };
+
+  submitHandler = e => {
+    const r1 = e.target.value;
+    const r2 = e.target.value;
+    const r3 = e.target.value;
+    const r4 = e.target.value;
+    const r5 = e.target.value;
+    const r6 = e.target.value;
+    const r7 = e.target.value;
+    const r8 = e.target.value;
+    const r9 = e.target.value;
+    const r10 = e.target.value;
+    const r11 = e.target.value;
+    const r12 = e.target.value;
+    const r13 = e.target.value;
+    const r14 = e.target.value;
+    const r15 = e.target.value;
+    const r16 = e.target.value;
+    console.log(
+      r1,
+      r2,
+      r3,
+      r4,
+      r5,
+      r6,
+      r7,
+      r8,
+      r9,
+      r10,
+      r11,
+      r12,
+      r13,
+      r14,
+      r15,
+      r16
+    );
+    console.log('Form Submitted');
+  };
+
+  render() {
+    return (
+      <div className={classes.CoursesDescription}>
+        <div className={classes.CoursesDescriptionHeader}>
+          <h3>Courses Description Process Form</h3>
+          <p>
+            <u>Institution: Government College University, Lahore</u>
+          </p>
+          <p>
+            <u>Program to be evaluated: Bachelor of Computer Science, BS(CS)</u>
+          </p>
+        </div>
+        <div className={classes.CoursesDescriptionArea}>
+          <table className={classes.CoursesDescriptionTable}>
+            <caption>
+              Subject: <strong>Compiler Construction</strong>
+            </caption>
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(this.state.data[0]).map(row => {
+                return (
+                  <tr key={row[0]}>
+                    <td>{row[1]}</td>
+                    <td>
+                      <TextArea
+                        placeholder={row[1]}
+                        name={row[0]}
+                        rows='8'
+                        onChange={this.onChange}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className={classes.buttonDiv}>
+            <Button onClick={this.submitHandler}>Submit Form</Button>
+          </div>
+        </div>
       </div>
-      <div className={classes.coursesDescriptionArea}></div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CoursesDescription;
--- src/components/Pages/CoursesMonitoring/CoursesMonitoring.js
+++ src/components/Pages/CoursesMonitoring/CoursesMonitoring.js
@@ -1,20 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './CoursesMonitoring.module.css';
+import Button from '../../UI/Button/Button';
+import TextArea from '../../UI/TextArea/TextArea';
 
-const CoursesMonitoring = () => {
-  return (
-    <div className={classes.CoursesMonitoring}>
-      <div className={classes.coursesMonitoringHeader}>
-        <h3>CoursesMonitoring Page</h3>
-        <p>
-          This is your CoursesMonitoring page. You can see the progress you've
-          made with your coursesMonitoring here and manage them
-        </p>
+class CoursesMonitoring extends Component {
+  state = {
+    r1: '',
+    r2: '',
+    r3: '',
+    r4: '',
+    r5: '',
+    data: {
+      0: {
+        r1: 'How far objectives have been achieved?',
+        r2: 'Full Coverage of contents',
+        r3: 'Relevant Problems Skills Development',
+        r4: 'Assessment Standards',
+        r5: 'Application of emerging technologies'
+      }
+    }
+  };
+
+  onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
+  };
+
+  submitHandler = () => {
+    const r1 = this.state.r1;
+    const r2 = this.state.r2;
+    const r3 = this.state.r3;
+    const r4 = this.state.r4;
+    const r5 = this.state.r5;
+    console.log('1', r1, '2', r2, '3', r3, '4', r4, '5', r5);
+    console.log('Form Submitted');
+  };
+
+  render() {
+    return (
+      <div className={classes.CoursesMonitoring}>
+        <div className={classes.CoursesMonitoringHeader}>
+          <h3>Courses Monitoring Process Form</h3>
+          <p>
+            <u>Institution: Government College University, Lahore</u>
+          </p>
+          <p>
+            <u>Program to be evaluated: Bachelor of Computer Science, BS(CS)</u>
+          </p>
+        </div>
+        <div className={classes.CoursesMonitoringArea}>
+          <table className={classes.CoursesMonitoringTable}>
+            <caption>
+              Subject: <strong>Compiler Construction</strong>
+            </caption>
+            <caption>
+              <strong>
+                A. Process to monitor the implementation of courses to ensure{' '}
+                the following important attributes:
+              </strong>
+            </caption>
+            <thead>
+              <tr>
+                <th>Criteria/Attribute</th>
+                <th>Existing Process</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(this.state.data[0]).map(row => {
+                return (
+                  <tr key={row[0]}>
+                    <td>{row[1]}</td>
+                    <td>
+                      <TextArea
+                        placeholder={row[1]}
+                        name={row[0]}
+                        rows='8'
+                        onChange={this.onChange}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className={classes.buttonDiv}>
+            <Button onClick={this.submitHandler}>Submit Form</Button>
+          </div>
+        </div>
       </div>
-      <div className={classes.coursesMonitoringArea}></div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CoursesMonitoring;
