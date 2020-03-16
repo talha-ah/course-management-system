06691458579e9b37203e7c8801e3f9f24f68527b
Talha <talha1392.ahmed@gmail.com>
2020-03-15 22:23:04 -0700

    404

--- src/components/Pages/404/404.js
+++ src/components/Pages/404/404.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const Error404 = () => {
-  return <h1>404</h1>;
+import Button from '../../UI/Button/Button';
+import classes from './pagefour.module.css';
+
+const onClickHandler = props => {
+  props.history.replace('/');
 };
 
-export default Error404;
+const Error404 = props => {
+  return (
+    <div className={classes.site}>
+      <h1>
+        404:
+        <small>Page Not Found</small>
+      </h1>
+      <div>
+        <Button type='button' onClick={() => onClickHandler(props)}>
+          Go Home
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(Error404);
