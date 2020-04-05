0418594f94a38d4038652bc8b4e98ba9a2effb79
Talha <talha1392.ahmed@gmail.com>
2020-04-05 02:31:47 -0700

    Profile Teacher Updated

--- src/components/Pages/TeacherPages/Profile/Profile.js
+++ src/components/Pages/TeacherPages/Profile/Profile.js
@@ -7,8 +7,12 @@ import Input from '../../../UI/Input/Input';
 
 class EditProfile extends React.Component {
   state = {
+    // Loadings
     pageLoading: true,
     isLoading: false,
+    // Tabs
+    tab: 'info',
+    // Personal Info
     firstName: '',
     lastName: '',
     email: '',
@@ -18,9 +22,11 @@ class EditProfile extends React.Component {
     country: '',
     city: '',
     zip: '',
+    // Password Inputs
     password: '',
     newPassword: '',
     confirmPassword: '',
+    // CV Input
     file: null,
     fileName: '',
   };
@@ -86,8 +92,8 @@ class EditProfile extends React.Component {
 
   onProfileUpdate = (e) => {
     e.preventDefault();
-
     this.setState({ isLoading: true });
+
     fetch(`${process.env.REACT_APP_SERVER_URL}/teacher/editteacher`, {
       method: 'POST',
       body: JSON.stringify({
@@ -136,6 +142,7 @@ class EditProfile extends React.Component {
   onPasswordChange = (e) => {
     e.preventDefault();
     this.setState({ isLoading: true });
+
     if (this.state.newPassword === this.state.confirmPassword) {
       fetch(`${process.env.REACT_APP_SERVER_URL}/teacher/editteacherpassword`, {
         method: 'POST',
@@ -174,14 +181,18 @@ class EditProfile extends React.Component {
           }
         });
     } else {
-      this.props.notify(true, 'Error', 'Passwords do not match!');
+      this.props.notify(
+        true,
+        'Error',
+        'New and Confirm passwords do not match!'
+      );
     }
   };
 
   onCVSubmit = (e) => {
     e.preventDefault();
-
     this.setState({ isLoading: true });
+
     if (this.state.file !== null && this.state.fileName !== '') {
       const file = this.state.file;
       const fileName = this.state.fileName;
@@ -236,187 +247,240 @@ class EditProfile extends React.Component {
       <Spinner size='Big' />
     ) : (
       <div className={classes.Profile}>
-        <div className={classes.Title}>
-          <h4>Your Profile</h4>
+        <div className={classes.Caption}>
+          <span className={classes.CaptionSpan}>
+            <strong>Your Profile</strong>
+          </span>
         </div>
-        <form
-          method='POST'
-          className={classes.Form}
-          onSubmit={this.onProfileUpdate}
-        >
-          <div className={classes.InputGroup}>
-            <div className={classes.InputDiv}>
-              <label htmlFor='firstName'>First Name</label>
-              <Input
-                type='text'
-                name='firstName'
-                placeholder='First Name'
-                value={this.state.firstName}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className={classes.InputDiv}>
-              <label htmlFor='lastName'>Last Name</label>
-              <Input
-                type='text'
-                name='lastName'
-                placeholder='Last Name'
-                value={this.state.lastName}
-                onChange={this.onChange}
-              />
-            </div>
-          </div>
-          <div className={classes.InputDiv}>
-            <label htmlFor='email'>Email Address</label>
-            <Input
-              type='email'
-              name='email'
-              placeholder='Email Address'
-              value={this.state.email}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className={classes.InputDiv}>
-            <label htmlFor='birthdate'>Birthdate</label>
-            <Input
-              type='date'
-              name='birthdate'
-              placeholder='Birthdate'
-              // value='1980-01-01'
-              value={this.state.birthdate}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className={classes.InputDiv}>
-            <label htmlFor='phone'>Phone Number</label>
-            <Input
-              type='number'
-              name='phone'
-              placeholder='Phone Number'
-              value={this.state.phone}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className={classes.InputDiv}>
-            <label htmlFor='address'>Address</label>
-            <Input
-              type='text'
-              name='address'
-              placeholder='Address'
-              value={this.state.address}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className={classes.InputGroup}>
-            <div className={classes.InputDiv}>
-              <label htmlFor='country'>Country</label>
-              <Input
-                type='text'
-                name='country'
-                placeholder='Country'
-                value={this.state.country}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className={classes.InputDiv}>
-              <label htmlFor='city'>City</label>
-              <Input
-                type='text'
-                name='city'
-                placeholder='City'
-                value={this.state.city}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className={classes.InputDiv}>
-              <label htmlFor='zip'>Zip</label>
-              <Input
-                type='text'
-                name='zip'
-                placeholder='Zip'
-                value={this.state.zip}
-                onChange={this.onChange}
-              />
-            </div>
-          </div>
-
-          <div className={classes.ButtonDiv}>
-            <Button type='submit'>
-              {this.state.isLoading ? 'Loading...' : 'Update Profile'}
-            </Button>
-          </div>
-        </form>
-        <br></br>
-        <div className={classes.Title}>
-          <h4>Change Password</h4>
-        </div>
-        <form
-          method='POST'
-          className={classes.Form}
-          onSubmit={this.onPasswordChange}
-        >
-          <div className={classes.InputDiv}>
-            <label htmlFor='password'>Current Password</label>
-            <Input
-              type='password'
-              name='password'
-              placeholder='Current Password'
-              value={this.state.password}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className={classes.InputDiv}>
-            <label htmlFor='newPassword'>New Password</label>
-            <Input
-              type='password'
-              name='newPassword'
-              placeholder='New Password'
-              value={this.state.newPassword}
-              onChange={this.onChange}
-            />
-          </div>
-          <div className={classes.InputDiv}>
-            <label htmlFor='confirmPassword'>Confirm Password</label>
-            <Input
-              type='password'
-              name='confirmPassword'
-              placeholder='Current Password'
-              value={this.state.confirmPassword}
-              onChange={this.onChange}
-            />
-          </div>
-
-          <div className={classes.ButtonDiv}>
-            <Button type='submit'>
-              {this.state.isLoading ? 'Loading...' : 'Update Password'}
-            </Button>
-          </div>
-        </form>
-        <br></br>
-        <div className={classes.Title}>
-          <h4>Update CV</h4>
-        </div>
-
-        <div className={classes.cvUpload}>
-          <form
-            method='POST'
-            onSubmit={this.onCVSubmit}
-            // encType='multipart/form-data'
+        <div className={classes.TabsButtons}>
+          <div
+            className={classes.Button}
+            onClick={() => this.setState({ tab: 'info' })}
+            style={{
+              borderBottom:
+                this.state.tab === 'info' ? '1px solid #3b3e66' : '',
+            }}
           >
-            <Input
-              type='file'
-              accept='application/pdf'
-              onChange={this.onChange}
-            />
-            <label htmlFor='file'>{this.state.fileName}</label>
-            <p>Drag your file here or click to select file.</p>
+            Change Personal Info
+          </div>
+          <div
+            className={classes.Button}
+            onClick={() => this.setState({ tab: 'cv' })}
+            style={{
+              borderBottom:
+                this.state.tab === 'cv' ? '1px solid #3b3e66' : 'none',
+            }}
+          >
+            Change CV
+          </div>
+          <div
+            className={classes.Button}
+            onClick={() => this.setState({ tab: 'password' })}
+            style={{
+              borderBottom:
+                this.state.tab === 'password' ? '1px solid #3b3e66' : 'none',
+            }}
+          >
+            Change Password
+          </div>
+        </div>
+        {this.state.tab === 'info' ? (
+          <form method='POST' onSubmit={this.onProfileUpdate}>
+            <div className={classes.InputGroup}>
+              <div className={classes.InputDiv}>
+                <label htmlFor='firstName'>First Name</label>
+                <Input
+                  type='text'
+                  name='firstName'
+                  placeholder='First Name'
+                  value={this.state.firstName}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className={classes.InputDiv}>
+                <label htmlFor='lastName'>Last Name</label>
+                <Input
+                  type='text'
+                  name='lastName'
+                  placeholder='Last Name'
+                  value={this.state.lastName}
+                  onChange={this.onChange}
+                />
+              </div>
+            </div>
+            <div className={classes.InputDiv}>
+              <label htmlFor='email'>Email Address</label>
+              <Input
+                type='email'
+                name='email'
+                placeholder='Email Address'
+                value={this.state.email}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className={classes.InputDiv}>
+              <label htmlFor='birthdate'>Birthdate</label>
+              <Input
+                type='date'
+                name='birthdate'
+                placeholder='Birthdate'
+                // value='1980-01-01'
+                value={this.state.birthdate}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className={classes.InputDiv}>
+              <label htmlFor='phone'>Phone Number</label>
+              <Input
+                type='number'
+                name='phone'
+                placeholder='Phone Number'
+                value={this.state.phone}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className={classes.InputDiv}>
+              <label htmlFor='address'>Address</label>
+              <Input
+                type='text'
+                name='address'
+                placeholder='Address'
+                value={this.state.address}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className={classes.InputGroup}>
+              <div className={classes.InputDiv}>
+                <label htmlFor='country'>Country</label>
+                <Input
+                  type='text'
+                  name='country'
+                  placeholder='Country'
+                  value={this.state.country}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className={classes.InputDiv}>
+                <label htmlFor='city'>City</label>
+                <Input
+                  type='text'
+                  name='city'
+                  placeholder='City'
+                  value={this.state.city}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className={classes.InputDiv}>
+                <label htmlFor='zip'>Zip</label>
+                <Input
+                  type='text'
+                  name='zip'
+                  placeholder='Zip'
+                  value={this.state.zip}
+                  onChange={this.onChange}
+                />
+              </div>
+            </div>
             <div className={classes.ButtonDiv}>
-              <Button type='submit'>
-                {this.state.isLoading ? 'Loading...' : 'Update CV'}
+              <Button
+                type='button'
+                buttonType='red'
+                onClick={() => this.props.history.push('/')}
+              >
+                Cancel
+              </Button>
+              <Button
+                type='submit'
+                disabled={this.state.isLoading ? true : false}
+              >
+                {this.state.isLoading ? 'Updating...' : 'Update Profile'}
               </Button>
             </div>
           </form>
-        </div>
+        ) : this.state.tab === 'cv' ? (
+          <div>
+            <div className={classes.cvUpload}>
+              <form
+                method='POST'
+                onSubmit={this.onCVSubmit}
+                // encType='multipart/form-data'
+              >
+                <Input
+                  type='file'
+                  accept='application/pdf'
+                  onChange={this.onChange}
+                />
+                <label htmlFor='file'>{this.state.fileName}</label>
+                <p>Drag your file here or click to select file.</p>
+              </form>
+            </div>
+            <div className={classes.ButtonDiv}>
+              <Button
+                type='button'
+                buttonType='red'
+                onClick={() => this.props.history.push('/')}
+              >
+                Cancel
+              </Button>
+              <Button
+                type='submit'
+                disabled={this.state.isLoading ? true : false}
+              >
+                {this.state.isLoading ? 'Uploading...' : 'Upload CV'}
+              </Button>
+            </div>
+          </div>
+        ) : this.state.tab === 'password' ? (
+          <form method='POST' onSubmit={this.onPasswordChange}>
+            <div className={classes.InputDiv}>
+              <label htmlFor='password'>Current Password</label>
+              <Input
+                type='password'
+                name='password'
+                placeholder='Current Password'
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className={classes.InputDiv}>
+              <label htmlFor='newPassword'>New Password</label>
+              <Input
+                type='password'
+                name='newPassword'
+                placeholder='New Password'
+                value={this.state.newPassword}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className={classes.InputDiv}>
+              <label htmlFor='confirmPassword'>Confirm Password</label>
+              <Input
+                type='password'
+                name='confirmPassword'
+                placeholder='Current Password'
+                value={this.state.confirmPassword}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className={classes.ButtonDiv}>
+              <Button
+                type='button'
+                buttonType='red'
+                onClick={() => this.props.history.push('/')}
+              >
+                Cancel
+              </Button>
+              <Button
+                type='submit'
+                disabled={this.state.isLoading ? true : false}
+              >
+                {this.state.isLoading ? 'Updating...' : 'Update Password'}
+              </Button>
+            </div>
+          </form>
+        ) : (
+          ''
+        )}
       </div>
     );
     return page;
